71a7e5d30f7faba941e8af08a02649611a6ac8ec
Irina Schmidke <irkos94@gmail.com>
2023-11-02 21:30:25 +0100

    make addition colculator

--- src/hallo.java
+++ src/hallo.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class hallo {
 
+
+
   public static void main(String[] args) {
-    System.out.println("Hallo world");
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+    System.out.println(a + b);
   }
 
 }
